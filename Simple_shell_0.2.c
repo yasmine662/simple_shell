08b15782ae8eaac0d310a2f91108a63b5f6f0e66
Yasmine Hsaini <youyasmine04@mail.com>
2023-05-20 11:50:53 -0700

    Simple_shell_0.2.c

---
+++ Simple_shell_0.2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#define MAX_ARGS 100
+/**
+ * main - handle  arguments
+ *
+ * Return: Always 0 in success
+ */
+int main(void)
+{
+        char *argv[MAX_ARGS];
+        int argc = MAX_ARGS;
+        int i;
+
+        FILE *Fl = fopen("handlearg.txt", "w");
+
+        if (Fl == NULL)
+        {
+                perror("opening Fl failed");
+                return (1);
+        }
+        for (i = 0; i < argc; i++)
+        {
+                if (argv[i] != NULL)
+                {
+                        fwrite(argv[i], sizeof(char), 1, Fl);
+                }
+        }
+        fclose(Fl);
+        return (0);
+}
