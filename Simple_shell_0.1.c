677966158e4a073f12679ab5ac435e3158f964f3
Malika <majerrad@gmail.com>
2023-05-21 20:01:11 -0700

    simple_shell

--- Simple_shell_0.1.c
+++ Simple_shell_0.1.c
@@ -16,34 +16,35 @@ int main(void)
 
 	if (my_shell == NULL)
 	{
-		perror("open file failed");
-		exit(1);
-	}
-	childpid = fork();
+		{
+			perror("open file failed");
+			exit(1);
+		}
+		childpid = fork();
 
-	if (childpid < 0)
-	{
-		perror("process to create failed");
-		return (1);
-	}
-	else if (childpid == 0)
-	{
-		char *const args[] = {"hsh", "my_shell", NULL};
+		if (childpid < 0)
+		{
+			perror("process to create failed");
+			return (1);
+		}
+		else if (childpid == 0)
+		{	
+			char *const args[] = {"hsh", "my_shell", NULL};
 
-		execve("/bin/hsh", args, NULL);
-		perror("execve failed");
-		return (1);
-	}
-	else
-	{
+			execve("/bin/hsh", args, NULL);
+			perror("execve failed");
+			return (1);
+		}
+		else
+		{
 		write(STDOUT_FILENO, "this is the ppid\n", 17);
 		wait(NULL);
+		}
 		while (fgets(command, sizeof(command), my_shell) != NULL)
 		{
-			fputs(command, stdout);
-		
-
-	}
+			fputs(command, stdout);		
+		}
 	fclose(my_shell);
+	}
 	return (0);
 }
