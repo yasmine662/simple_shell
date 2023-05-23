968b6d2d08b5e56e0307138936f8235d1f543386
Malika <majerrad@gmail.com>
2023-05-23 15:40:16 -0700

    simple_shell

--- Simple_shell_0.1.c
+++ Simple_shell_0.1.c
@@ -1,7 +1,10 @@
-#include "shell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-#define STDOUT_FILENO 1
-#define MAX_COMMAND_LENGTH 100
 /**
  * main - a UNIX command line interpreter.
  *
@@ -10,41 +13,58 @@
 
 int main(void)
 {
-	FILE *my_shell = fopen("my_shell", "r");
+	char command[1024];
+	char const prompt[] = "#cisfun$ ";
+	int  length = 0;
 	pid_t childpid;
-	char command[MAX_COMMAND_LENGTH];
 
-	if (my_shell == NULL)
+	while (1)
+        {
+        write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
+  
+	if (fgets(command, sizeof(command), stdin) == NULL)
 	{
-		{
-			perror("open file failed");
-			exit(1);
-		}
-		childpid = fork();
+	break;
+	}
+
+	length = 0;
+	while (command[length] != '\0' && command[length] != '\n')
+	{
+		length++;
+	}
+        if (length == 0 || (length == 1 && command[0] == '\n'))
+	{
+		break;
+	}
+        if (command[length - 1] == '\n')
+	{
+		command[length - 1] = '\0';
+	}
+	childpid = fork();
 
 		if (childpid < 0)
 		{
 			perror("process to create failed");
 			return (1);
 		}
-		else if (childpid == 0)
-		{	
-			char *const args[] = {"ls", NULL};
-
-			execve("/bin/ls", args, NULL);
-			perror("execve failed");
-			return (1);
-		}
+		else 
+		if (childpid == 0)
+		{
+			char *args[2];
+			args[0] = "/bin/ls";
+			args[1] = "NULL";
+			
+			if (execve(command, args, NULL) == -1)
+			{
+				write(STDERR_FILENO, "./shell: ", 9);
+				write(STDERR_FILENO, ": No such file or directory\n", 28);
+                        	return (1);
+			}
+                }
 		else
 		{
-		write(STDOUT_FILENO, "this is the ppid\n", 17);
-		wait(NULL);
+			wait(NULL);
 		}
-		while (fgets(command, sizeof(command), my_shell) != NULL)
-		{
-			fputs(command, stdout);		
-		}
-	fclose(my_shell);
 	}
 	return (0);
 }
